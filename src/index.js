7456abd9132a7f18d7ab148f29716ce4a98567b9
andreygp5 <andreygp555@gmail.com>
2021-01-25 18:58:18 +0300

    feat: add array towel sort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix == undefined) return [];
+    let towelSortMatrix = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (!(i % 2)) {
+            towelSortMatrix.push(...matrix[i]);
+        } else {
+            towelSortMatrix.push(...matrix[i].reverse());
+        }
+    }
+    return towelSortMatrix;
+};
